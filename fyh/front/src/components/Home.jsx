b420052123282bd6edb592f327f9d1c04c441987
Ramiro Grisales noguera <ramirogrisales@gmail.com>
2022-08-02 08:19:57 -0500

    new changes

---
+++ fyh/front/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CardsTimer from './CardsTimer'
+
+function Home(props) {
+    return (
+        <div>
+            desde el home
+            <CardsTimer/>
+        </div>
+    );
+}
+
+export default Home
---
+++ fyh/front/src/redux/constans.js
@@ -0,0 +1,4 @@
+export const ACTION_TEST = 'ACTION_TEST'
+export const START = 'START'
+export const ADD_TIMER ='ADD_TIMER'
+export const API_URL = 'http://localhost:8081'
