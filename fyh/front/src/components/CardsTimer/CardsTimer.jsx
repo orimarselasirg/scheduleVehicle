dacc715485a3cbe997d3b114bc089eed6e8f58cd
Ramiro <ramirogrisales@gmail.com>
2022-08-06 13:01:11 -0500

    adds

--- fyh/front/src/components/CardsTimer/CardsTimer.jsx
+++ fyh/front/src/components/CardsTimer/CardsTimer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Timer from '../Timer/Timer'
 import {useDispatch} from 'react-redux'
 import {useState} from 'react'
+import './cardstimer.css'
 
 function CardsTimer(props) {
     const dispatch = useDispatch()
@@ -25,13 +26,25 @@ function CardsTimer(props) {
     }
     return (
         <div>
-            <form>
-            <input type='text' placeholder ='Placas del vehiculos' name = 'id_vehicle' onChange={inputChanger}/>
-            <label>Placa</label>
-            <input type='text' placeholder ='Bahia de trabajo' name = 'pit'onChange={inputChanger}/>
-            <label>Bahia</label>
-            <input type='text' placeholder ='Placas del vehiculos' name = 'driver'onChange={inputChanger}/>
-            <label>Conductor</label>
+            <form className='field is-grouped'>
+                <div className='field has-addons app'>
+                    <div className='control'>
+                        <input className = 'input is-small' type='text' placeholder ='Placas del vehiculos' name = 'id_vehicle' onChange={inputChanger}/>
+                    </div>
+                    {/* <label className='button is-static'>Placa</label> */}
+                </div>
+                <div className='field has-addons'>
+                    <div className='control'>
+                        <input className = 'input is-small' type='text' placeholder ='Bahia de trabajo' name = 'pit'onChange={inputChanger}/>
+                    </div>
+                    {/* <label className='button is-static'>Bahia</label> */}
+                </div>
+                <div className='field has-addons'>
+                    <div className='control'>
+                        <input  className = 'input is-small' type='text' placeholder ='conductor' name = 'driver'onChange={inputChanger}/>
+                    </div>
+                    {/* <label className='button is-static'>Conductor</label> */}
+                </div>
             <input className = 'button is-small is-dark' type="button" value="Set Timer" onClick={activateTimer} />       
             </form>
             <div className='container'>
